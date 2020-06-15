29af0e5b1bd6429db57d8c21d254fe378cdcb3db
cmartinc22 <martin.castro@pedidosya.com>
2020-06-15 16:57:40 -0300

    Update

--- lib/fastlane/plugin/check_coverage_ios/actions/check_coverage_ios_action.rb
+++ lib/fastlane/plugin/check_coverage_ios/actions/check_coverage_ios_action.rb
@@ -6,6 +6,15 @@ module Fastlane
     class CheckCoverageIosAction < Action
       def self.run(params)
         UI.message("The check_coverage_ios plugin is working!")
+        UI.message(params)
+        # xcov(
+        #   workspace: "testdc.xcworkspace",
+        #   scheme: "testdc",
+        #   output_directory: "xcov_output",
+        #   minimum_coverage_percentage: min_coverage,
+        #   json_report: true,
+        #   html_report: false
+        # )
       end
 
       def self.description
@@ -27,6 +36,17 @@ module Fastlane
 
       def self.available_options
         [
+          FastlaneCore::ConfigItem.new(key: :min_coverage,
+                                  env_name: "CHECK_COVERAGE_IOS_MIN_COVERAGE",
+                               description: "Min Coverage Required",
+                                  optional: true,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :module_name,
+                                  env_name: "CHECK_COVERAGE_IOS_MODULE_NAME",
+                               description: "Module Name",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :your_option
           # FastlaneCore::ConfigItem.new(key: :your_option,
           #                         env_name: "CHECK_COVERAGE_IOS_YOUR_OPTION",
           #                      description: "A description of your option",
